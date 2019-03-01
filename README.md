02fce84fe39882def3886a9c483fac0b5b7fee13
Mutasim Issa <mutasim@sourcya.com>
2019-03-01 08:55:33 +0100

    V: 0.0.3

--- README.md
+++ README.md
@@ -23,10 +23,16 @@ npm install
 ionic serve -c
 ```
 
+- Test dev APK on Android emulator/device
+```sh
+ionic cordova run android -l
+```
+
 #### Running Tests
 TODO
 
 #### Build for Production
+
 ##### PWA:
 
 ```sh
@@ -35,19 +41,36 @@ ionic build --prod
 Currently We disable service worker from the production build or --prod will fail
 
 ##### Andriod:
-TODO
+See [Ionic4 docs about Android setup](https://ionicframework.com/docs/installation/android)
+```sh
+ionic cordova build android --prod
+```
 
 #### References
 > This project is me learning Ionic4, I used the following resources (Updated):
 
+##### API
 - [Ionic4 with API calls Tutorial](https://medium.freecodecamp.org/how-to-build-your-first-ionic-4-app-with-api-calls-f6ea747dc17a)
+
+##### Angular
 - [Angular router](https://angular.io/guide/router)
 - [Angular Observables](https://angular.io/guide/observables)
-- [MDN mixed content](https://developer.mozilla.org/en-US/docs/Web/Security/Mixed_content)
 - [Angular Service workers docs](https://angular.io/guide/service-worker-getting-started)
+
+##### PWA
+- [MDN mixed content](https://developer.mozilla.org/en-US/docs/Web/Security/Mixed_content)
 - [Good tutorial on hosting Ionic PWA on firebase](https://www.joshmorony.com/hosting-an-ionic-pwa-with-firebase-hosting/)
 - [How PWAs were done in ionic3 DEPRECATED in ionic4 see Stencil Below](https://blog.ionicframework.com/how-to-make-pwas-with-ionic/)
-- [Saved Me lot of time understanding the environment changes while building Ionic for prod](https://www.joshmorony.com/production-development-environment-variables-in-ionic-angular/)
+- [Saved Me lot of time understanding the environment changes while building Ionic PWA for prod](https://www.joshmorony.com/production-development-environment-variables-in-ionic-angular/)
+
+##### StencilJS
 - [StencilJS -> If Cordova plugins are the native Mobile Interface of Ionic, then Stencil Components are the webview Interface of Ionic](https://stenciljs.com/)
+
+##### Example Projects
 - [Example Project: Ionic Conference App](https://github.com/ionic-team/ionic-conference-app)
 - [Example project: Ionic Insta Music](https://github.com/jgw96/instamusic)
+
+##### Problems
+- SOLVED [Failed to load resource: net::ERR_FILE_NOT_FOUND ionic error](https://stackoverflow.com/questions/37449936/failed-to-load-resource-neterr-file-not-found-ionic-error)
+- UNSOLVED [Ionic4 apps don’t work with Android 4.x](https://ionic.zone/debug/ionic-and-android-4)
+-
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 // import { timer } from 'rxjs/observable/timer';
-//
+
 // import { Network } from '@ionic-native/network/ngx';
 
 @Component({
@@ -18,56 +18,59 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar,
+    private statusBar: StatusBar
     // private network: Network,
   )
   {
     this.initializeApp();
   }
 
-  splash()
-  {
-    if(this.platform.is('desktop')){
-      console.log('%cFIXME: Should call a preloader for Desktop instead of Spalsh','color:red');
-      //Should initiate a preloader here for Desktop browsers
-    }
-    else {
-      this.splashScreen.hide();
-      // timer(3000).subscribe(()=>{this.showSplash = false});
-    }
-  }
-
-  netOnOrOff()
-  {
-    if(this.platform.is('desktop')){
-      console.log('%cFIXME: Should do a different Network detect. for Desktop','color:red;');
-      // over here, Cordova network info doesn't support Desktop
-    }
-    else {
-      console.log('FIXME: Other platforms not Desktop, need network detection method')
-      // this.network.onDisconnect().subscribe(() => {alert('Disconnected');});
-      // this.network.onConnect().subscribe(()=> {alert('Connected');});
-    }
-  }
-
-  statusBarOnMobile()
-  {
-    if(this.platform.is('desktop')){
-      console.log('%cDesktop detected -> Not Calling statusBar styleDefault method','color:blue;');
-      // over here, Cordova network info doesn't support Desktop
-    }
-    else {
-      this.statusBar.styleDefault();
-    }
-  }
+  // splash()
+  // {
+  //   if(this.platform.is('desktop')){
+  //     console.log('%cFIXME: Should call a preloader for Desktop instead of Spalsh','color:red');
+  //     //Should initiate a preloader here for Desktop browsers
+  //   }
+  //   else {
+  //     this.splashScreen.hide();
+  //     timer(3000).subscribe(()=>{this.showSplash = false});
+  //   }
+  // }
+  //
+  // netOnOrOff()
+  // {
+  //   if(this.platform.is('desktop')){
+  //     console.log('%cFIXME: Should do a different Network detect. for Desktop','color:red;');
+  //     // over here, Cordova network info doesn't support Desktop
+  //   }
+  //   else {
+  //     console.log('FIXME: Other platforms not Desktop, need network detection method')
+  //     this.network.onDisconnect().subscribe(() => {alert('Disconnected');});
+  //     this.network.onConnect().subscribe(()=> {alert('Connected');});
+  //   }
+  // }
+  //
+  // statusBarOnMobile()
+  // {
+  //   if(this.platform.is('desktop')){
+  //     console.log('%cDesktop detected -> Not Calling statusBar styleDefault method','color:blue;');
+  //     // over here, Cordova network info doesn't support Desktop
+  //   }
+  //   else {
+  //     this.statusBar.styleDefault();
+  //   }
+  // }
 
   initializeApp()
   {
       this.platform.ready().then(() => {
 
-        this.statusBarOnMobile();
-        this.splash();
-        this.netOnOrOff();
+        // this.statusBarOnMobile();
+        // this.splash();
+        // this.netOnOrOff();
+
+        this.statusBar.styleDefault();
+        this.splashScreen.hide();
     });
   }
 
